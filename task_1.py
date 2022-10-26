df6f87dca081da40b08b0b4c33ad62136a43d4d5
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-10-26 17:14:44 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,16 +1,23 @@
 def remove_parentheses(text):
 
     x=False
+    chuj=False
     text2=""
 
     for i in range(len(text)):
+
+        if chuj==True and text[i]==" ":
+            continue
 
         if text[i] == "(" :
             x = True
 
         if x==False:
             text2+=text[i]
-            
+        
+        if (x==True) and (text[i] == ")"):
+            chuj=True
+
         if text[i] == ")" :
             x = False
 
