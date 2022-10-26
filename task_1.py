8a326974ae3186b32169111a229066d1fa2d5483
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-10-26 17:03:12 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,17 @@
 def remove_parentheses(text):
-    return text
+
+    x=False
+    text2=""
+
+    for i in range(len(text)):
+
+        if text[i] == "(" :
+            x = True
+
+        if x==False:
+            text2+=text[i]
+            
+        if text[i] == ")" :
+            x = False
+
+remove_parentheses(text)
