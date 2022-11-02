fddc6c06692c853d8329feb1552faa4fb2e833f0
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-11-02 19:47:31 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,24 +1,20 @@
 def is_diabolic(n):
-    if "666" in str(n):
-        print(n)
-        return True
-    else:
-        return False
+    "666" in str(n):
 
 def is_prime(n):
     if n<2:
         return False
-    else:
-        for i in range(2,n-1):
-            if (n%i)==0:    
-                return False
-            else:
-                return True
+    for i in range(2,n-1):
+        if (n%i)==0:    
+            return False
+    return True
+
 d=0
 
 for i in range(100000):
-    if is_prime(i)==True:  
+    if is_prime(i)==True:
         if is_diabolic(i)==True:
+            print(n)
             d+=1
 print (d)
 
