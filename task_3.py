8bfbc22e7e78a8ad3fcb929176be400ef5f482a4
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-11-02 19:57:22 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -4,7 +4,7 @@ def is_diabolic(n):
 def is_prime(n):
     if n<2:
         return False
-    for i in range(2,n-1):
+    for i in range(2,(n/2)+1):
         if (n%i)==0:    
             return False
     return True
