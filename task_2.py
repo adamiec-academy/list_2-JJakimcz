456fcaf35783359e80551d639c0fbe90008d1ce5
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-11-02 11:17:09 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,27 @@
 
+from random import randint
+from turtle import *
+import turtle
+
+inflation_rate = 20
+tracer(0,1)
+for _ in range(20):
+    inflation_rate +=  randint(-10, 40)
+    while True:
+        turtle.forward(2)
+        turtle.left(90)
+
+        turtle.forward(inflation_rate)
+        turtle.left(90)
+
+        turtle.forward(2)
+        turtle.left(90)
+
+        turtle.forward(inflation_rate)
+        turtle.left(90)
+        break
+    turtle.penup()
+    turtle.forward(10)
+    turtle.pendown()
+    update()   
+exitonclick()
