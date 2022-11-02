df48485a451aeed50764e05afe4b5c7c39980605
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-11-02 12:16:37 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,30 +1,26 @@
-n=100000
-
 def is_diabolic(n):
-    x=0
-    sigma=str(n)[:-3]
-    if sigma=="666":
-        x = x + 1
-        print ("liczba diabelska: ",n)
-        print ("która: ",x)
+    if "666" in str(n):
+        print(n)
         return True
     else:
-        print("liczba nie diabelska: ",n)
         return False
 
 def is_prime(n):
-    for n in range(2,100000):
+    if n<2:
+        return False
+    else:
         for i in range(2,n-1):
             if (n%i)==0:    
-                print("liczba nie pierwsza przy dzieleniu: ",n)
-                print("przez: ",i)
+                return False
             else:
-                break
-            print("liczba pierwsza nie",n)
-            is_diabolic(n) 
+                return True
+d=0
 
-
-is_prime(n)
+for i in range(100000):
+    if is_prime(i)==True:  
+        if is_diabolic(i)==True:
+            d+=1
+print (d)
 
 
 
