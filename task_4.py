43b2784f25cd17c6f123fcf4b35bf084bb98926f
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-11-02 11:10:48 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,12 +1,12 @@
 def multiplication_table(x1, x2, y1, y2):
-        max_spaces = len(str(x2*y2))+1
+    max_spaces = len(str(x2*y2))+1
     i=y1
 
     print(" " * (max_spaces+1) +"|",end="")
 
-    while y2>=i:
-        print(f"{i : {max_spaces}}",end="")
-        i=i+1
+    for y in range(y1, y2 + 1):
+        print(f"{y : {max_spaces}}",end="")
+    
     print("")
     print("−" * (max_spaces+1) * y2 ,end="")
 
